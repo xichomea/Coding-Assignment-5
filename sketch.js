35d744ffbca0712a8d65caa7015d914fbc0468f1
xichomea <xichomea@gmail.com>
2025-11-12 00:19:38 +0900

    movers 배열 만들기
    mover 여러 개 만들고 화면에 모두 표시. (객체당 힘이 어떻게 다르게 작용하는지 비교할 수 있음)

--- sketch.js
+++ sketch.js
@@ -1,7 +1,26 @@
+let movers = [];
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(350, 600);
+  colorMode(HSB, 360, 100, 100, 100); 
+
+  for (let i = 0; i < 5; i++) {
+    movers[i] = new Mover(random(0.5, 2)); 
+  }
 }
 
+
 function draw() {
-  background(220);
+  background('#C5E2FF');
+  let buoyancy = createVector(0, -0.02); 
+  let wind = createVector(random(-0.01, 0.01), 0); 
+
+  for (let mover of movers) {
+    mover.applyForce(buoyancy);
+    mover.applyForce(wind); 
+
+    mover.update();
+    mover.show();
+    mover.pass();
+  }  
 }
